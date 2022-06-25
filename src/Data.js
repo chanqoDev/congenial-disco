54820abe5e047796094765906ecfd3d7fb9f6556
Christian E <christianelena92@gmail.com>
2022-06-24 19:19:34 -0500

    add Icons and Items objects

--- src/Data.js
+++ src/Data.js
@@ -1,21 +1,55 @@
 const items = [
   {
+    title: "Amazon Web Services Logo",
     image: "Amazon-aws.png",
   },
   {
-    image: "item-2.png",
+    title: "Firebase Icon",
+    image: "Firebase_Icon.png",
   },
   {
-    image: "item-3.png",
+    title: "HTML 5 Icon",
+    image: "HTML5.png",
   },
   {
-    image: "item-4.png",
+    title: "JS Icon",
+    image: "JS_SquareIcon.png",
   },
   {
-    image: "item-5.png",
+    title: "Material UI Icon",
+    image: "Material-ui.png",
   },
   {
-    image: "item-6.png",
+    title: "MongoDB Icon",
+    image: "MongoDB-Icon.png",
+  },
+  {
+    title: "Node Icon",
+    image: "Node_icon.png",
+  },
+  {
+    title: "NPM Icon",
+    image: "Npm.png",
+  },
+  {
+    title: "React Icon",
+    image: "React_Logo_2.png",
+  },
+  {
+    title: "Redux Icon",
+    image: "Redux_icon.png",
+  },
+  {
+    title: "Serverless Icon",
+    image: "Serverless.png",
+  },
+  {
+    title: "Typescript Icon",
+    image: "Typescript.png",
+  },
+  {
+    title: "Yarn Icon",
+    image: "Yarn.png",
   },
 ];
 
